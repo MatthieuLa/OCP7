e52ac35d11a3d9463ddf0a75dca89736ea094dfe
Matt <160069337+MatthieuLa@users.noreply.github.com>
2024-06-01 12:47:37 +0200

    Code du composant Collapse et de la page About. Style du composant Collapse.

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,8 +1,36 @@
-export default function Collapse() {
+import React, { useState } from "react";
+
+export default function Collapse({ items }) {
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const handleToggle = (index) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
   return (
-    <div className="collapse">
-      <p></p>
-      <i className="fa-solid fa-chevron-up"></i>
+    <div className="collapse-container">
+      {items.map((item, index) => (
+        <div key={index} className="collapse-wrapper">
+          <div
+            className="collapse-title collapse"
+            onClick={() => handleToggle(index)}
+          >
+            {item.title}
+            <i
+              className={`fa-solid ${
+                openIndex === index ? "fa-chevron-down" : "fa-chevron-up"
+              }`}
+            ></i>
+          </div>
+          {openIndex === index && (
+            <div className="collapse-bg">
+              <div className="collapse-content">
+                <p>{item.content}</p>
+              </div>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,35 @@
 import Collapse from "../components/Collapse";
 
 export default function About() {
+  const collapseItems = [
+    {
+      title: "Fiabilité",
+      content:
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
+    },
+    {
+      title: "Respect",
+      content:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de pertubation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      title: "Service",
+      content:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de pertubation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      title: "Sécurité",
+      content:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ];
+
   return (
-    <>
+    <div>
       <div className="img-overlay">
         <img src="./src/assets/images/03.png" alt="" className="img-home" />
       </div>
-      <Collapse />
-      <Collapse />
-      <Collapse />
-      <Collapse />
-    </>
+      <Collapse items={collapseItems} />
+    </div>
   );
 }
