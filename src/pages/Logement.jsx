c7df05325e815a5a8ddbf956d81d223a0d4fb26a
Matt <160069337+MatthieuLa@users.noreply.github.com>
2024-06-12 10:14:27 +0200

    responsive

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -21,56 +21,58 @@ export default function Logement() {
 
   return (
     <>
-      <Slideshow pictures={logement.pictures} />
-      <div className="content-wrapper">
-        <div className="top-wrapper">
-          <div className="top-description">
-            <h1 className="log-title">{logement.title}</h1>
-            <p className="log-location">{logement.location}</p>
+      <div className="main-container">
+        <Slideshow pictures={logement.pictures} />
+        <div className="content-wrapper">
+          <div className="top-wrapper">
+            <div className="top-description">
+              <h1 className="log-title">{logement.title}</h1>
+              <p className="log-location">{logement.location}</p>
 
-            <div className="log-tags__wrapper">
-              {logement.tags.map((tag, index) => (
-                <span key={index} className="log-tag">
-                  {tag}
-                </span>
-              ))}
+              <div className="log-tags__wrapper">
+                {logement.tags.map((tag, index) => (
+                  <span key={index} className="log-tag">
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="top-host__container">
+              <div className="top-host__top">
+                <p className="log-host-name">{logement.host.name}</p>
+                <img
+                  src={logement.host.picture}
+                  alt={logement.host.name}
+                  className="log-host"
+                />
+              </div>
+              <div className="log-rating__wrapper">
+                {[...Array(5)].map((star, i) => {
+                  const ratingValue = i + 1;
+                  return (
+                    <i
+                      key={i}
+                      className={`fa-solid fa-star ${
+                        ratingValue <= logement.rating ? "clr-on" : "clr-off"
+                      }`}
+                    ></i>
+                  );
+                })}
+              </div>
             </div>
           </div>
-          <div className="top-host__container">
-            <div className="top-host__top">
-              <p className="log-host-name">{logement.host.name}</p>
-              <img
-                src={logement.host.picture}
-                alt={logement.host.name}
-                className="log-host"
-              />
-            </div>
-            <div className="log-rating__wrapper">
-              {[...Array(5)].map((star, i) => {
-                const ratingValue = i + 1;
-                return (
-                  <i
-                    key={i}
-                    className={`fa-solid fa-star ${
-                      ratingValue <= logement.rating ? "clr-on" : "clr-off"
-                    }`}
-                  ></i>
-                );
-              })}
-            </div>
+          <div className="collapse-logement">
+            <Collapse title={"Description"}>
+              <p>{logement.description}</p>
+            </Collapse>
+            <Collapse title={"Equipements"}>
+              <ul>
+                {logement.equipments.map((equipment) => (
+                  <li key={equipment}>{equipment}</li>
+                ))}
+              </ul>
+            </Collapse>
           </div>
-        </div>
-        <div className="collapse-logement">
-          <Collapse title={"Description"}>
-            <p>{logement.description}</p>
-          </Collapse>
-          <Collapse title={"Equipements"}>
-            <ul>
-              {logement.equipments.map((equipment) => (
-                <li key={equipment}>{equipment}</li>
-              ))}
-            </ul>
-          </Collapse>
         </div>
       </div>
     </>
